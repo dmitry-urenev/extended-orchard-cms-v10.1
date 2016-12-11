2c8144553f2f0c36b90003b8bc50f1681d95012a
Dmitry Urenev <dmitry.urenev@gmail.com>
2016-12-11 12:34:10 +0100

    Some useful features: Confirmation in widgets "delete with translations" action, Show page title settings, MenuItemLink fix (removed strange invisible symbol)

--- src/Orchard.Web/Core/Title/Models/TitlePart.cs
+++ src/Orchard.Web/Core/Title/Models/TitlePart.cs
@@ -4,10 +4,17 @@ using Orchard.ContentManagement.Aspects;
 
 namespace Orchard.Core.Title.Models {
     public class TitlePart : ContentPart<TitlePartRecord>, ITitleAspect {
+
         [Required]
         public string Title {
             get { return Retrieve(x => x.Title); }
             set { Store(x => x.Title, value); }
+        }
+
+        public bool RenderTitle
+        {
+            get { return Retrieve(x => x.RenderTitle); }
+            set { Store(x => x.RenderTitle, value); }
         }
     }
 }
--- src/Orchard.Web/Core/Title/Models/TitlePartRecord.cs
+++ src/Orchard.Web/Core/Title/Models/TitlePartRecord.cs
@@ -2,8 +2,16 @@
 using Orchard.ContentManagement.Records;
 
 namespace Orchard.Core.Title.Models {
-    public class TitlePartRecord : ContentPartVersionRecord {
+    public class TitlePartRecord : ContentPartVersionRecord
+    {
+        public TitlePartRecord()
+        {
+            RenderTitle = true;
+        }
+
         [StringLength(1024)]
         public virtual string Title { get; set; }
+
+        public virtual bool RenderTitle { get; set; }
     }
 }
